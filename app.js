7c3d46d8c5e76d38ac085414234d876af0080d21
eightseconds <paulseokcho@gmail.com>
2019-07-13 20:01:25 -0700

    Backend user auth

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const mongoose = require('mongoose')
-;
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
     .connect(db, { userNewUrlParser: true})
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use(passport.initialize());
+require('./config/passport')(passport)
+
 
 const port = process.env.PORT || 5000;
 
