5fc7e84638c8d29c95ec883efb211d7f74833fd4
eightseconds <paulseokcho@gmail.com>
2019-07-15 14:35:53 -0700

    Complete workout plan show page css

--- frontend/src/components/home/home_page.jsx
+++ frontend/src/components/home/home_page.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import logo from './logo.png'
+import plus from './plus.png'
+import menu from './menu.png'
+import Modal from 'react-modal';
 import './home_page.css'
 
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
+
+
+        this.state = {
+            modalIsOpen: false
+        };
+
         this.logoutUser = this.logoutUser.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     logoutUser(e) {
@@ -13,22 +25,65 @@ class HomePage extends React.Component {
         this.props.logout();
     }
 
+    openModal() {
+        this.setState({ modalIsOpen: true });
+    }
+
+    afterOpenModal() {
+        this.subtitle.style.color = '#f00';
+    }
+
+    closeModal() {
+        this.setState({ modalIsOpen: false });
+    }
+
+
     render() {
         return (
                 <div className="liftur--ui">
+                    <button onClick={this.openModal}>  
+                        <img src={menu} alt="Menu" className="menu"/>
+                    </button>
                     <div className="home-page--top">
                         <div className="home-page--header">
-                            <img src={logo} alt="Logo" className="logo" />
-                            <h1>Liftur</h1>
+                            <div>
+                                <Modal
+                                    isOpen={this.state.modalIsOpen}
+                                    className="home-page--modal"
+                                >
+                                    <div className="home-page--modal--closer">
+                                        <button onClick={this.closeModal} className='modal-close'>x</button>
+                                    </div>
+                                    <div className="home-page--modal--container">
+                                        <div className="home-page--modal--items">
+                                            Plan 1
+                                        </div>
+                                        <div className="home-page--modal--items">
+                                            Plan 2
+                                        </div>
+                                        <div className="home-page--modal--items">
+                                            Plan 3
+                                        </div>
+                                    </div>
+                                    <div className="home-page--modal--logout">
+                                        <button onClick={this.logoutUser}>Log Out</button>   
+                                    </div>
+                                </Modal>
+                            </div>
+                            <div className="header--logo">
+                                <img src={logo} alt="Logo" className="logo" />
+                                <h1>Liftur</h1>
+                            </div>
                         </div>
-                        <button onClick={this.logoutUser}>Log Out</button>   
                     </div>
                     <div className="plan--wrapper">
-                        <div className="plan--container">
+                        <div className="plan--header">
                             Plan 1
                         </div>
-                        <div className="plan--container">
-                            Create New Plan
+                        <div className="plan--scrollable">
+                        </div>
+                        <div className="plan--footer">
+                            <img src={plus} alt="Plus" className="plus" />
                         </div>
                     </div>
                 </div>
